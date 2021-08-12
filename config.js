de71616b1d071b3a92a6d4215ef2665abbec2b48
Diego F. Prado <goidesdesign@gmail.com>
2021-08-12 12:09:06 -0500

    [add] Config

---
+++ config.js
@@ -0,0 +1,32 @@
+module.exports = {
+    remoteDB: process.env.REMOTE_DB || false,
+    api: {
+        port: process.env.API_PORT || 3000
+    },
+    posts: {
+        port: process.env.POSTS_PORT || '3002'
+    },
+    jwt: {
+        secret: process.env.SECRET || 'secreto'
+    },
+    mysql:{
+        host: process.env.MYSQL_HOST || 'remotemysql.com',
+        user: process.env.MYSQL_USER || 'RLgNJF8hIB',
+        password: process.env.MYSQL_PASSWORD || 'xKoanJlCro',
+        database: process.env.MYSQL_DATABASE || 'RLgNJF8hIB',
+        port: process.env.MYSQL_PORT || '3306',
+    },
+    mysql_service:{
+        host: process.env.MYSQL_SERVICE_HOST || 'localhost',
+        port: process.env.MYSQL_SERVICE_PORT || '3001',
+    },
+    cache_service:{
+        host: process.env.MYSQL_SERVICE_HOST || 'localhost',
+        port: process.env.CACHE_SERVICE_PORT || '3006'
+    },
+    redis: {
+        host: process.env.REDIS_HOST || 'redis-16702.c51.ap-southeast-2-1.ec2.cloud.redislabs.com',
+        port: process.env.REDIS_PORT || '16702',
+        password: process.env.REDIS_PASSWORD || 'fWSfg2GRQdhy2z77xVaxgPfjoPTnsqlU'
+    }
+}
